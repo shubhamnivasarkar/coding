883e41d64a7a5e80ca71094b51f4a8d56e9a7757
Shubham Nivasarkar <74969994+shubhamnivasarkar@users.noreply.github.com>
2021-11-20 09:54:59 +0530

    Create SortedLinkedListIntf.java

---
+++ assignments/NavenduSir/Day5/src/implementation/SortedLinkedListIntf.java
@@ -0,0 +1,8 @@
+package implementation;
+
+public interface SortedLinkedListIntf {
+	public void Insert(int element);
+	public void Delete(int element);
+	public int[] GetAllElements();
+	void AddAtFront(int element);
+}
